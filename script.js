43bba9370d5840842c7699863062c97cd80f69ee
Riaz-khan-8121 <riazkakar8121940@gmail.com>
2025-09-16 10:51:41 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+// Use prompt() to ask for the user's name.
+// Use alert() to display:
+// "Welcome, [name]!"
+// Concepts used: prompt, alert, var, concatenation
+
+// let userName=prompt("Enter your Name...")
+// alert("welcome "+userName)
+
+
+
+
+
+// 🔸 Assignment 2: Age Message
+// Objective:
+// Ask the user for their name and age using prompt().
+// Log this message in the console:
+// "[name] is [age] years old."
+
+
+// let userName=prompt("Enter your Name...")
+// let userAge=prompt("Enter your age...")
+
+// console.log(userName +" " + " is " +userAge+" " +" years old")
+
+
+
+
+
+// 🔸 Assignment 3: Favorite Color
+// Objective:
+// Ask the user for their favorite color.
+// Show an alert like:
+// "Wow! [color] is a nice color!"
+
+
+// let userColor=prompt("Enter your favorite color")
+// alert("wow " + userColor +" is a nice color!")
+
+
+
+// Assignment 4: Simple Math Sentence
+// Objective:
+// Ask the user for two numbers using prompt().
+// Show a sentence in the console like:
+
+// "The result of 4 + 5 is 9."
+// (You can just add the numbers directly — no need to convert types yet.)
+
+
+let firstNum=parseInt(prompt("Enter your first num..."))
+let secondNum=parseInt(prompt("Enter your second num..."))
+console.log("the result of "+firstNum +"+"+secondNum +" is "+(firstNum+secondNum))
